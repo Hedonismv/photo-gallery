aae86159f5e74217bffb5d8db8740aa90c5bfc56
Vitaly Klyhun <klygun90@gmail.com>
2022-03-23 17:03:27 +0300

    react missing fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from "react";
 import Header from "./Components/Header/Header";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {useCollection} from 'react-firebase-hooks/firestore'
