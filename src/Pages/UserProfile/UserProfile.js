26158d14f2e2f4ae65db0383c572f760932923a8
Vitaly Klyhun <klygun90@gmail.com>
2022-04-01 18:37:09 +0300

    Rename and litteraly refactoring

--- src/Pages/UserProfile/UserProfile.js
+++ src/Pages/UserProfile/UserProfile.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './userProfile.css';
+import {useSelector} from "react-redux";
 
 
 const UserProfile = () => {
+
+	const {loggedUser} = useSelector(state => state.authReducer)
+
 	return (
 		<div className={'container'}>
-			<h1>Profile</h1>
+			{loggedUser ?
+				<div className={'profile_container'}>
+					<h1>Hello, {loggedUser.displayName}</h1>
+					<div className={'profile_counter_block'}>
+						<span className={'profile_followers'}>Followers: {loggedUser.followers.length}</span>
+						<span className={'profile_following'}>Following: {loggedUser.following.length}</span>
+					</div>
+				</div>
+				: <h1>Loading...</h1>
+			}
 		</div>
 	);
 };
