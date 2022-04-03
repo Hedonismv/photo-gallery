50ca83130dbe9c6b706dbbf4ef84db60767c10d7
Vitaly Klyhun <klygun90@gmail.com>
2022-04-03 17:39:17 +0300

    remove api

--- src/firebaseConfig/firestoreConfig.js
+++ src/firebaseConfig/firestoreConfig.js
@@ -2,22 +2,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getStorage} from "firebase/storage"
-import {getDatabase} from "firebase/database"
 import {getAuth, GoogleAuthProvider} from "firebase/auth";
 import {collection, getFirestore} from "firebase/firestore";
+import {firebaseConfig} from "./settings";
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-	apiKey: "AIzaSyD7a7MQUHPin3ENt2sy-ZTloJf3A1wabL0",
-	authDomain: "photogallery-bc9d2.firebaseapp.com",
-	projectId: "photogallery-bc9d2",
-	storageBucket: "photogallery-bc9d2.appspot.com",
-	messagingSenderId: "122911408378",
-	appId: "1:122911408378:web:f8222bcfe65f2797524943"
-};
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
