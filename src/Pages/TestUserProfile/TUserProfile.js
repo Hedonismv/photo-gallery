e3a60b85e84dfbeabefa80d539d1a38f2804c931
Vitaly Klyhun <klygun90@gmail.com>
2022-04-04 21:49:45 +0300

    subs func refactoring and popup vision toggle

--- src/Pages/TestUserProfile/TUserProfile.js
+++ src/Pages/TestUserProfile/TUserProfile.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './tUserProfile.css';
 import {useDocumentData} from "react-firebase-hooks/firestore";
-import {arrayRemove, arrayUnion, doc, updateDoc} from "firebase/firestore";
+import {doc} from "firebase/firestore";
 import {projectFirestore} from "../../firebaseConfig/firestoreConfig";
 import {useParams} from "react-router";
 import ImageList from "../../Components/ImageList/ImageList";
 import {useSelector} from "react-redux";
+import UsersList from "../../Components/UsersList/UsersList";
+import {handleUnsubscribe, handleSubscribe} from "../../helpers/subsHandlers";
 
 
 const TUserProfile = () => {
@@ -14,41 +16,21 @@ const TUserProfile = () => {
 
 	const {loggedUser} = useSelector(state => state.authReducer)
 
+	const [visible, setVisible] = useState(false)
+	const [setting, setSetting] = useState("")
+
 	//Firestore const
 	const profileUserRef = doc(projectFirestore, 'users', params.id)
 	const loggedUserRef = loggedUser ? doc(projectFirestore, 'users', loggedUser.id) : ''
 
 
-	const docRef = doc(projectFirestore, 'users', params.id)
-	const [userProfileData, loading, error] = useDocumentData(docRef)
+	const [userProfileData, loading, error] = useDocumentData(profileUserRef)
 	console.log(userProfileData)
 
-
-	const handleSubscribe = () => {
-
-		updateDoc(loggedUserRef, {
-			following: arrayUnion(params.id)
-		})
-			.catch(error => console.log(error));
-
-		updateDoc(profileUserRef, {
-			followers: arrayUnion(loggedUser.id)
-		})
-			.catch(error => console.log(error));
+	const handleVisible = (setting) => {
+		setSetting(setting)
+		setVisible(true)
 	}
-
-	const handleUnsubscribe = () => {
-		updateDoc(loggedUserRef, {
-			following: arrayRemove(params.id)
-		})
-			.catch(error => console.log(error));
-
-		updateDoc(profileUserRef, {
-			followers: arrayRemove(loggedUser.id)
-		})
-			.catch(error => console.log(error));
-	}
-
 
 	if(loading){
 		return (
@@ -68,6 +50,7 @@ const TUserProfile = () => {
 
 	return (
 		<div className={'container'}>
+			{visible ? <UsersList usrData={userProfileData} setting={setting} setVisible={setVisible}/> : null}
 			<div className={'user-profile_container'}>
 				<div className={'user-profile_top_container'}>
 					<div className={'user-profile_vis_info'}>
@@ -76,14 +59,24 @@ const TUserProfile = () => {
 					</div>
 					<div className={'user-profile_sub_btn-container'}>
 						{loggedUser.following.includes(params.id) ?
-							<button className={'user-profile-sub-btn'} onClick={handleUnsubscribe}>Unsubscribe</button>
-							: <button className={'user-profile-sub-btn'} onClick={handleSubscribe}>Subscribe</button>
+							<button
+								className={'user-profile-sub-btn'}
+								onClick={() => handleUnsubscribe(loggedUserRef,profileUserRef, params.id, loggedUser.id)}
+							>
+								Unsubscribe
+							</button>
+							: <button
+								className={'user-profile-sub-btn'}
+								onClick={() => handleSubscribe(loggedUserRef,profileUserRef, params.id, loggedUser.id)}
+							>
+								Subscribe
+							</button>
 						}
 					</div>
 				</div>
 				<div className={'profile_subs_container'}>
-					<span className={'profile_followers'}>Followers: {userProfileData.followers.length}</span>
-					<span className={'profile_following'}>Following: {userProfileData.following.length}</span>
+					<span className={'profile_followers'} onClick={() => handleVisible("followers")}>Followers: {userProfileData.followers.length}</span>
+					<span className={'profile_following'} onClick={() => handleVisible("following")}>Following: {userProfileData.following.length}</span>
 				</div>
 			</div>
 			<ImageList profileView={true}/>
