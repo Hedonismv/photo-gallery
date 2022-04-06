2ea5fc9d12c1fbe1ef1420e7a59b53fc60920337
Vitaly Klyhun <klygun90@gmail.com>
2022-04-06 21:14:35 +0300

    add direct route

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {Navigate, Route, Routes} from "react-router";
 import Main from "../Pages/Main/Main";
-import UserImages from "../Pages/UserImages/UserImages";
 import UserProfile from '../Pages/UserProfile/UserProfile';
 import TUserProfile from "../Pages/TestUserProfile/TUserProfile";
+import DirectMessages from "../Pages/DirectMessages/DirectMessages";
 
 const PrivateRoutes = () => {
 	return (
 		<Routes>
 			<Route path={'/'} element={<Main/>}/>
-			<Route path={'/my-images'} element={<UserImages/>}/>
+			<Route path={'/direct'} element={<DirectMessages/>}/>
 			<Route path={'/my-profile'} element={<UserProfile/>}/>
 			<Route path={'/profile/:id'} element={<TUserProfile/>}/>
 			<Route path={'*'} element={<Navigate to={'/'}/>}/>
