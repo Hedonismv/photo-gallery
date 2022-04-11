4ee9aa93af43b54af06aa77e2b7203bd1dc37b22
Vitaly Klyhun <klygun90@gmail.com>
2022-04-11 14:26:42 +0300

    created page and styling it

--- src/Pages/ImagePage/ImagePage.js
+++ src/Pages/ImagePage/ImagePage.js
@@ -4,6 +4,7 @@ import {doc} from "firebase/firestore";
 import {projectFirestore} from "../../firebaseConfig/firestoreConfig";
 import {useDocumentData} from "react-firebase-hooks/firestore";
 import Comments from "../../Components/Comments/Comments";
+import './imagePage.css';
 
 const ImagePage = () => {
 	const params = useParams()
@@ -27,9 +28,29 @@ const ImagePage = () => {
 	}
 
 	return (
-		<div className={'container'}>
-			<h1>Image Page with id: {params.id}</h1>
-			<h1>Likes: {imageData.likes}</h1>
+		<div className={'container adaptive_padding'}>
+			<div className={'img_page_main'}>
+				<div className={'image_list_grid_column'}>
+					<div className={'img_page_image_object'}>
+						<img src={imageData.url} alt={'image_Page'}/>
+					</div>
+				</div>
+				<div className={'image_list_grid_column'}>
+					<div className={'img_page_image_object'}>
+						<div className={'img_page_author-info'}>
+							<img className={'header_user_avatar'} src={imageData.authorPhotoUrl} alt={'userPhoto'}/>
+							<p className={'regular'}>{imageData.authorDisplayName}</p>
+						</div>
+						<div className={'img_page_author_date'}>
+							<span className={'grey'}>{new Date(imageData.createdAt).toLocaleDateString()}</span>
+							<span className={'grey'}>{new Date(imageData.createdAt).toLocaleTimeString()}</span>
+						</div>
+					</div>
+					<div className={'img_page_author-likes'}>
+						<span>Likes: {imageData.likes}</span>
+					</div>
+				</div>
+			</div>
 			<div>
 				<Comments/>
 			</div>
